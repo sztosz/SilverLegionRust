3746db2a241631ccecaa312a956d9dc6455f6b63
Bartosz Nowak <sztosz@gmail.com>
2024-03-17 12:48:54 +0100

    Add dead soldiers to output.

--- src/main.rs
+++ src/main.rs
@@ -136,10 +136,70 @@ fn main() {
         None => println!("No bases found in save file"),
     };
 
+    match docs[1].get("deadSoldiers") {
+        Some(soldiers) => match soldiers.as_sequence() {
+            Some(soldiers) => {
+                for soldier in soldiers {
+                    let name = soldier.get("name").unwrap().as_str().unwrap();
+                    let id = soldier.get("id").unwrap().as_i64().unwrap();
+                    println!("Found dead soldier {} {}", id, name);
+                    let missions = soldier.get("missions").unwrap().as_i64().unwrap();
+                    let kills = soldier.get("kills").unwrap().as_i64().unwrap();
+                    let stuns = soldier.get("stuns").unwrap().as_i64().unwrap();
+
+                    let days_wounded = match soldier.get("diary").unwrap().get("daysWoundedTotal") {
+                        Some(val) => val.as_i64().unwrap(),
+                        None => 0,
+                    };
+
+                    let current_stats = soldier.get("currentStats").unwrap();
+
+                    let bravery = current_stats.get("bravery").unwrap().as_i64().unwrap();
+                    let firing = current_stats.get("firing").unwrap().as_i64().unwrap();
+                    let health = current_stats.get("health").unwrap().as_i64().unwrap();
+                    let mana = current_stats.get("mana").unwrap().as_i64().unwrap();
+                    let melee = current_stats.get("melee").unwrap().as_i64().unwrap();
+                    let psi_skill = match current_stats.get("psiSkill") {
+                        Some(val) => val.as_i64().unwrap(),
+                        None => 0,
+                    };
+                    let psi_strength = current_stats.get("psiStrength").unwrap().as_i64().unwrap();
+                    let reactions = current_stats.get("reactions").unwrap().as_i64().unwrap();
+                    let strength = current_stats.get("strength").unwrap().as_i64().unwrap();
+                    let throwing = current_stats.get("throwing").unwrap().as_i64().unwrap();
+                    let tu = current_stats.get("tu").unwrap().as_i64().unwrap();
+
+                    let soldier_stats = Soldier {
+                        name: name.to_owned(),
+                        id: id,
+                        missions: missions,
+                        kills: kills,
+                        stuns: stuns,
+                        days_wounded: days_wounded,
+                        bravery: bravery,
+                        firing: firing,
+                        health: health,
+                        mana: mana,
+                        melee: melee,
+                        psi_skill: psi_skill,
+                        psi_strength: psi_strength,
+                        reactions: reactions,
+                        strength: strength,
+                        throwing: throwing,
+                        tu: tu,
+                    };
+
+                    soldiers_data.push(soldier_stats)
+                }
+            }
+            None => println!("No bases found in save file"),
+        },
+        None => println!("No dead soldiers found"),
+    };
+
     soldiers_data.sort_by(|a, b| a.id.cmp(&b.id));
 
     let mut wtr = csv::Writer::from_path(out_filename).unwrap();
-    
 
     for record in soldiers_data {
         wtr.serialize(record).unwrap();
