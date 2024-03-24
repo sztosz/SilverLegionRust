369e0860340072c40774a7a4d129b647668bb35a
Bartosz Nowak <sztosz@gmail.com>
2024-03-24 15:19:54 +0100

    Extract parsing soldier data to seprate function

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,91 @@ struct Soldier {
     initial_stamina: i64,
 }
 
+impl Soldier {
+    fn from_data(soldier_data: &Value) -> Soldier {
+        let name = soldier_data.get("name").unwrap().as_str().unwrap();
+        let armor = soldier_data.get("armor").unwrap().as_str().unwrap();
+        let id = soldier_data.get("id").unwrap().as_i64().unwrap();
+        println!("Found alive soldier {} {}", id, name);
+        let missions = soldier_data.get("missions").unwrap().as_i64().unwrap();
+        let kills = soldier_data.get("kills").unwrap().as_i64().unwrap();
+        let stuns = soldier_data.get("stuns").unwrap().as_i64().unwrap();
+
+        let days_wounded = match soldier_data.get("diary").unwrap().get("daysWoundedTotal") {
+            Some(val) => val.as_i64().unwrap(),
+            None => 0,
+        };
+
+        let current_stats = soldier_data.get("currentStats").unwrap();
+        let initial_stats = soldier_data.get("initialStats").unwrap();
+
+        let stamina = current_stats.get("stamina").unwrap().as_i64().unwrap();
+        let initial_stamina = initial_stats.get("stamina").unwrap().as_i64().unwrap();
+        let bravery = current_stats.get("bravery").unwrap().as_i64().unwrap();
+        let initial_bravery = initial_stats.get("bravery").unwrap().as_i64().unwrap();
+        let firing = current_stats.get("firing").unwrap().as_i64().unwrap();
+        let initial_firing = initial_stats.get("firing").unwrap().as_i64().unwrap();
+        let health = current_stats.get("health").unwrap().as_i64().unwrap();
+        let initial_health = initial_stats.get("health").unwrap().as_i64().unwrap();
+        let mana = current_stats.get("mana").unwrap().as_i64().unwrap();
+        let initial_mana = initial_stats.get("mana").unwrap().as_i64().unwrap();
+        let melee = current_stats.get("melee").unwrap().as_i64().unwrap();
+        let initial_melee = initial_stats.get("melee").unwrap().as_i64().unwrap();
+        let psi_skill = match current_stats.get("psiSkill") {
+            Some(val) => val.as_i64().unwrap(),
+            None => 0,
+        };
+        let initial_psi_skill = match initial_stats.get("psiSkill") {
+            Some(val) => val.as_i64().unwrap(),
+            None => 0,
+        };
+        let psi_strength = current_stats.get("psiStrength").unwrap().as_i64().unwrap();
+        let initial_psi_strength = initial_stats.get("psiStrength").unwrap().as_i64().unwrap();
+        let reactions = current_stats.get("reactions").unwrap().as_i64().unwrap();
+        let initial_reactions = initial_stats.get("reactions").unwrap().as_i64().unwrap();
+        let strength = current_stats.get("strength").unwrap().as_i64().unwrap();
+        let initial_strength = initial_stats.get("strength").unwrap().as_i64().unwrap();
+        let throwing = current_stats.get("throwing").unwrap().as_i64().unwrap();
+        let initial_throwing = initial_stats.get("throwing").unwrap().as_i64().unwrap();
+        let initial_tu = initial_stats.get("tu").unwrap().as_i64().unwrap();
+        let tu = current_stats.get("tu").unwrap().as_i64().unwrap();
+
+        Soldier {
+            name: name.to_owned(),
+            armor: armor.to_owned(),
+            id,
+            missions,
+            kills,
+            stuns,
+            days_wounded,
+            initial_stamina,
+            stamina,
+            initial_bravery,
+            bravery,
+            initial_firing,
+            firing,
+            initial_health,
+            health,
+            mana,
+            initial_mana,
+            melee,
+            initial_melee,
+            initial_psi_skill,
+            psi_skill,
+            psi_strength,
+            initial_psi_strength,
+            reactions,
+            initial_reactions,
+            initial_strength,
+            strength,
+            initial_throwing,
+            throwing,
+            initial_tu,
+            tu,
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
@@ -76,89 +161,9 @@ fn main() {
                     .expect("could not find base name!!!");
                 match base.get("soldiers") {
                     Some(soldiers) => match soldiers.as_sequence() {
-                        Some(soldiers) => for soldier in soldiers {
-                            let name = soldier.get("name").unwrap().as_str().unwrap();
-                            let armor = soldier.get("armor").unwrap().as_str().unwrap();
-                            let id = soldier.get("id").unwrap().as_i64().unwrap();
-                            println!("Found alive soldier {} {}", id, name);
-                            let missions = soldier.get("missions").unwrap().as_i64().unwrap();
-                            let kills = soldier.get("kills").unwrap().as_i64().unwrap();
-                            let stuns = soldier.get("stuns").unwrap().as_i64().unwrap();
+                        Some(soldiers) => for soldier_data in soldiers {
 
-                            let days_wounded = match soldier.get("diary").unwrap().get("daysWoundedTotal") {
-                                Some(val) => val.as_i64().unwrap(),
-                                None => 0,
-                            };
-
-                            let current_stats = soldier.get("currentStats").unwrap();
-                            let initial_stats = soldier.get("initialStats").unwrap();
-
-                            let stamina = current_stats.get("stamina").unwrap().as_i64().unwrap();
-                            let initial_stamina = initial_stats.get("stamina").unwrap().as_i64().unwrap();
-                            let bravery = current_stats.get("bravery").unwrap().as_i64().unwrap();
-                            let initial_bravery = initial_stats.get("bravery").unwrap().as_i64().unwrap();
-                            let firing = current_stats.get("firing").unwrap().as_i64().unwrap();
-                            let initial_firing = initial_stats.get("firing").unwrap().as_i64().unwrap();
-                            let health = current_stats.get("health").unwrap().as_i64().unwrap();
-                            let initial_health = initial_stats.get("health").unwrap().as_i64().unwrap();
-                            let mana = current_stats.get("mana").unwrap().as_i64().unwrap();
-                            let initial_mana = initial_stats.get("mana").unwrap().as_i64().unwrap();
-                            let melee = current_stats.get("melee").unwrap().as_i64().unwrap();
-                            let initial_melee = initial_stats.get("melee").unwrap().as_i64().unwrap();
-                            let psi_skill = match current_stats.get("psiSkill") {
-                                Some(val) => val.as_i64().unwrap(),
-                                None => 0,
-                            };
-                            let initial_psi_skill = match initial_stats.get("psiSkill") {
-                                Some(val) => val.as_i64().unwrap(),
-                                None => 0,
-                            };
-                            let psi_strength = current_stats.get("psiStrength").unwrap().as_i64().unwrap();
-                            let initial_psi_strength = initial_stats.get("psiStrength").unwrap().as_i64().unwrap();
-                            let reactions = current_stats.get("reactions").unwrap().as_i64().unwrap();
-                            let initial_reactions = initial_stats.get("reactions").unwrap().as_i64().unwrap();
-                            let strength = current_stats.get("strength").unwrap().as_i64().unwrap();
-                            let initial_strength = initial_stats.get("strength").unwrap().as_i64().unwrap();
-                            let throwing = current_stats.get("throwing").unwrap().as_i64().unwrap();
-                            let initial_throwing = initial_stats.get("throwing").unwrap().as_i64().unwrap();
-                            let initial_tu = initial_stats.get("tu").unwrap().as_i64().unwrap();
-                            let tu = current_stats.get("tu").unwrap().as_i64().unwrap();
-
-                            let soldier_stats = Soldier {
-                                name: name.to_owned(),
-                                armor: armor.to_owned(),
-                                id,
-                                missions,
-                                kills,
-                                stuns,
-                                days_wounded,
-                                initial_stamina,
-                                stamina,
-                                initial_bravery,
-                                bravery,
-                                initial_firing,
-                                firing,
-                                initial_health,
-                                health,
-                                mana,
-                                initial_mana,
-                                melee,
-                                initial_melee,
-                                initial_psi_skill,
-                                psi_skill,
-                                psi_strength,
-                                initial_psi_strength,
-                                reactions,
-                                initial_reactions,
-                                initial_strength,
-                                strength,
-                                initial_throwing,
-                                throwing,
-                                initial_tu,
-                                tu,
-                            };
-
-                            soldiers_data.push(soldier_stats)
+                            soldiers_data.push(Soldier::from_data(soldier_data))
                         },
                         None => println!("No bases found in save file"),
                     },
@@ -174,92 +179,8 @@ fn main() {
 
     match docs[1].get("deadSoldiers") {
         Some(soldiers) => match soldiers.as_sequence() {
-            Some(soldiers) => for soldier in soldiers {
-                let name = soldier.get("name").unwrap().as_str().unwrap();
-                let armor = soldier.get("armor").unwrap().as_str().unwrap();
-                let id = soldier.get("id").unwrap().as_i64().unwrap();
-                println!("Found dead soldier {} {}", id, name);
-                let missions = soldier.get("missions").unwrap().as_i64().unwrap();
-                let kills = soldier.get("kills").unwrap().as_i64().unwrap();
-                let stuns = soldier.get("stuns").unwrap().as_i64().unwrap();
-
-                let days_wounded = match soldier.get("diary").unwrap().get("daysWoundedTotal") {
-                    Some(val) => val.as_i64().unwrap(),
-                    None => 0,
-                };
-
-                let current_stats = soldier.get("currentStats").unwrap();
-                let initial_stats = soldier.get("initialStats").unwrap();
-
-                let stamina = current_stats.get("stamina").unwrap().as_i64().unwrap();
-                let initial_stamina = initial_stats.get("stamina").unwrap().as_i64().unwrap();
-                let bravery = current_stats.get("bravery").unwrap().as_i64().unwrap();
-                let initial_bravery = initial_stats.get("bravery").unwrap().as_i64().unwrap();
-                let firing = current_stats.get("firing").unwrap().as_i64().unwrap();
-                let initial_firing = initial_stats.get("firing").unwrap().as_i64().unwrap();
-                let health = current_stats.get("health").unwrap().as_i64().unwrap();
-                let initial_health = initial_stats.get("health").unwrap().as_i64().unwrap();
-                let mana = current_stats.get("mana").unwrap().as_i64().unwrap();
-                let initial_mana = initial_stats.get("mana").unwrap().as_i64().unwrap();
-                let melee = current_stats.get("melee").unwrap().as_i64().unwrap();
-                let initial_melee = initial_stats.get("melee").unwrap().as_i64().unwrap();
-                let psi_skill = match current_stats.get("psiSkill") {
-                    Some(val) => val.as_i64().unwrap(),
-                    None => 0,
-                };
-                let initial_psi_skill = match initial_stats.get("psiSkill") {
-                    Some(val) => val.as_i64().unwrap(),
-                    None => 0,
-                };
-                let psi_strength = current_stats.get("psiStrength").unwrap().as_i64().unwrap();
-                let initial_psi_strength = initial_stats.get("psiStrength").unwrap().as_i64().unwrap();
-                let reactions = current_stats.get("reactions").unwrap().as_i64().unwrap();
-                let initial_reactions = initial_stats.get("reactions").unwrap().as_i64().unwrap();
-                let strength = current_stats.get("strength").unwrap().as_i64().unwrap();
-                let initial_strength = initial_stats.get("strength").unwrap().as_i64().unwrap();
-                let throwing = current_stats.get("throwing").unwrap().as_i64().unwrap();
-                let initial_throwing = initial_stats.get("throwing").unwrap().as_i64().unwrap();
-                let initial_tu = initial_stats.get("tu").unwrap().as_i64().unwrap();
-                let tu = current_stats.get("tu").unwrap().as_i64().unwrap();
-
-
-
-
-                let soldier_stats = Soldier {
-                    name: name.to_owned(),
-                    armor: armor.to_owned(),
-                    id,
-                    missions,
-                    kills,
-                    stuns,
-                    days_wounded,
-                    initial_stamina,
-                    stamina,
-                    initial_bravery,
-                    bravery,
-                    initial_firing,
-                    firing,
-                    initial_health,
-                    health,
-                    mana,
-                    initial_mana,
-                    melee,
-                    initial_melee,
-                    initial_psi_skill,
-                    psi_skill,
-                    psi_strength,
-                    initial_psi_strength,
-                    reactions,
-                    initial_reactions,
-                    initial_strength,
-                    strength,
-                    initial_throwing,
-                    throwing,
-                    initial_tu,
-                    tu,
-                };
-
-                dead_soldiers_data.push(soldier_stats)
+            Some(soldiers) => for soldier_data in soldiers {
+                dead_soldiers_data.push(Soldier::from_data(soldier_data))
             },
             None => println!("No bases found in save file"),
         },
